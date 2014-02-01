497f748c8e35287cb2a386d52a73c51ab7bedfb1
unknown <admin1@VIT-WINL05C-06.wdc.vidyalankarlive.com>
2014-02-01 16:07:20 +0530

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Project1::Application.routes.draw do
+  resources :users
+
+  get "say/hello"
+  get "say/goodbie"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
