4688b79261932453abb0041c60ccd9a9e28edca8
unknown <admin1@VIT-WINL05C-06.wdc.vidyalankarlive.com>
2014-02-02 15:06:16 +0530

    finish login and logout

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+	def create
+		@post=Post.new(post_params)
+		@post.user_id=current_user.id
+
+		if @post.save
+			redirect_to current_user
+		else
+			flash[:error]="oh NO"
+			redirect_to current_user
+		end
+	end
+	private
+	def post_params
+		params.require(:post).permit(:content,:user_id)
+		
+	end
+end
